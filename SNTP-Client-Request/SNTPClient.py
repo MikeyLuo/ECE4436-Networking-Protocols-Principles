6a2bfd82f0cc026b423565e3a1930bf09482014e
MikeyLuo <hluong22@uwo.ca>
2020-10-25 23:11:56 -0400

    Added more to the client code, and set socket settings

--- SNTP-Client-Request/SNTPClient.py
+++ SNTP-Client-Request/SNTPClient.py
@@ -3,26 +3,19 @@ import struct
 import sys
 import time
 
-#NTP_SERVER =
+NTP_SERVER = 'pool.ntp.org'
 TIME1970 = 2208989900 #Time stamp in refernce to January 1, 1970 in the format 1970-01-01 00:00:00
 
-def sntp_client(host="pool.ntp.org"): # Defining the NTP server which we are connecting to 
- ''' Fill in start 
-
-    let variable data contain the received data
-    let address be the address that the NTP replied from 
-    
-Fill in end'''
+def sntp_client(): # Defining the NTP server which we are connecting to 
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    data = str.encode('\x1b'+47*'\0')
+    client.sendto(data,(NTP_SERVER,123))
+    data,addr = client.recvfrom(1024)
 
     if data:
-        print('Response received from:', address)
+        print('Response received from:', addr)
+    t=struct.unpack('!12I',data)[10] - TIME1970
+    print('\tTime = %s' % time.ctime(t))
         
-'''Fill in start
-
-        in this part you have to calculate the time
-       let variable t be the final time you calculated  
-
-Fill in end'''
-        #print ('\tTime=%s' % time.ctime(t))
 if __name__ == '__main__':
     sntp_client()
