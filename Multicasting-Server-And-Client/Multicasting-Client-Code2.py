247f721a3dc6d3c22c2833eb2e62cd28253dba07
MikeyLuo <hluong22@uwo.ca>
2020-11-25 15:34:24 -0500

    Fixed small errors that was casuing the server to write to the client every 1/1000th of a second. Now it only writes every 3 seconds like it should

--- Multicasting-Server-And-Client/Multicasting-Client-Code2.py
+++ Multicasting-Server-And-Client/Multicasting-Client-Code2.py
@@ -3,23 +3,19 @@ import struct
 import sys
 
 def clientCode():
-    #multicast_group = '192.168.0.27'
-    #erverAddy = ('',5000)
-
-    # Create the socket
+   
     sockServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,socket.IPPROTO_UDP)
-     # flag to tell the program to resuse a local socket so it doesn't have to wait for the timeout
+    
     sockServer.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) 
-    # Bind the socket to the server address 
+   
     sockServer.bind(('',5007))
 
-    # Tell program to add the socket created to the multicast group
-    #group = socket.inet_aton(multicast_group)
+    
     mreq = struct.pack('=4sl',socket.inet_aton('224.1.1.1'),socket.INADDR_ANY)
     sockServer.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
      
 
-    # check for recevied data and then respond
+   
     while True:
         try:
             data, address = sockServer.recvfrom(1024)
--- Multicasting-Server-And-Client/Multicasting-Server-Code2.py
+++ Multicasting-Server-And-Client/Multicasting-Server-Code2.py
@@ -6,17 +6,14 @@ import time
 
 def serverCode():
 
-    #message = input(b' Multicasting Assignmnet ECE 4436/9303 from server 1 ') # creating the message that server will send
-    multicast_group = ('224.1.1.1',5007) # setting the IP address and port number
+   
+    multicast_group = ('224.1.1.1',5007) 
 
-    sockServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # creating the datagram socket
-    sockServer.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255) # setting the data packet TTL for each message 
-    # set timeout to 3 seconds so program knows how many responses to expect
+    sockServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) 
+    sockServer.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 255)  
+      
     sockServer.settimeout(3)
 
-   
-   
-    # checks for any responses from all the clients
     while True:
         sockServer.sendto(b' Multicasting Assignmnet ECE 4436/9303 from server 2 ', multicast_group)
         print  ('Server 2: multicast packet is sent now')
